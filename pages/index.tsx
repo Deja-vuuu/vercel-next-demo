da2905468759d442f44598cb269f36d317ab2af6
杨阳 <1002522146@qq.com>
2022-08-02 18:20:10 +0800

    :sparkles: 删除antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React, { useState } from 'react';
 
-import 'antd/dist/antd.css';
 
 const Home: NextPage = () => {
   const [visible, setVisible] = useState(false);
