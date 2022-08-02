8ac85026fba2d9ef46918d6c30f8bd25f7581bb7
杨阳 <1002522146@qq.com>
2022-08-02 18:25:27 +0800

    :sparkles: 提交测试

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React, { useState } from 'react';
 
 
 const Home: NextPage = () => {
-  const [visible, setVisible] = useState(false);
-
-  const showDrawer = () => {
-    setVisible(true);
-  };
-
-  const onClose = () => {
-    setVisible(false);
-  };
 
   return (
     <div className={styles.container}>
-      <Head>
+      <h1>
         test
-      </Head>
+      </h1>
     </div>
   )
 }
