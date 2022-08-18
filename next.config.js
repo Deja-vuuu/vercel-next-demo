ed6fe5baf2d5cfffea9fa36fc81dcd5e3262bb67
杨阳 <1002522146@qq.com>
2022-08-18 19:45:14 +0800

    :sparkles: 提交代码

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictode: true,
   swcMinify: true,
+  reactStrictMode: true,
+
+  // 实验性配置
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 }
 
 module.exports = nextConfig
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,29 +1,42 @@
 import { useRouter } from "next/router";
-import type { NextPage } from 'next'
 
-const Detail: NextPage = props => {
-  // const { data } = props
-  // console.log('data', data);
+import Image from "next/image"
+import type { NextPage } from 'next'
+import { useEffect } from "react";
+
+const Detail: NextPage = (props: any) => {
+  const { data } = props
+  console.log('data', data);
+  console.log('data', data?.sprites?.other);
 
   const router = useRouter();
   const { id } = router?.query;
+
+  useEffect(() => {
+    
+  }, [])
   return (
     <h1>
       {id}
+      {
+        data?.name
+      }
+      <Image src={data?.sprites?.other?.["official-artwork"]?.front_default}
+        width={200} height={200} />
     </h1>
   )
 }
 
 
-// export async function getServerSideProps(context) {
-//   const { id } = context.query;
-//   const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id).then((data) => {
-//     return data.json();
-//   });
-//   return {
-//     props: {
-//       data: res
-//     }
-//   };
-// }
+export async function getServerSideProps(context: any) {
+  const { id } = context.query;
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id).then((data) => {
+    return data.json();
+  });
+  return {
+    props: {
+      data: res
+    }
+  };
+}
 export default Detail;
