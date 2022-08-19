128755622d8f56a2fb7aa8416708de420fa083c7
杨阳 <1002522146@qq.com>
2022-08-19 14:59:53 +0800

    :sparkles: 测试测试

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 const Detail: NextPage = (props: any) => {
   const { data } = props
   console.log('data', data);
-  console.log('da1ta', data?.sprites?.other);
+  console.log('da11t1a', data?.sprites?.other);
 
   const router = useRouter();
   const { id } = router?.query;
