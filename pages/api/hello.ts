03117998198e0a124cb5c636b7fa3eb315d85af7
杨阳 <1002522146@qq.com>
2022-08-22 19:08:14 +0800

    feat: 提交

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { renderToString } from 'react-dom/server'
 
 type Data = {
   name: string
 }
 
+const a = () => {
+  return renderToString(<div>Hello World </div>)
+}
+
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'Jhn Doe' })
+  res.status(200).send(`<html> <head> <title>express ssr</title> </head> <body> <h1>Hello SSR</h1> ${a()}</body> </html>`)
 }
