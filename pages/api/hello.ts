fee457272066cb334dd5484c3f2d00fa34156a1c
杨阳 <1002522146@qq.com>
2022-08-23 19:35:38 +0800

    feat: 提交

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -6,14 +6,15 @@ type Data = {
   name: string
 }
 
-const a = () => {
-  return renderToString(<div>Hello World </div>)
-}
+// const a = () => {
+//   return renderToString(<div>Hello World </div>)
+// }
 
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).send(`<html> <head> <title>express ssr</title> </head> <body> <h1>Hello SSR</h1> ${a()}</body> </html>`)
+  // res.status(200).send(`<html> <head> <title>express ssr</title> </head> <body> <h1>Hello SSR</h1> }</body> </html>`)
+  res.status(200).json({ name: 'John Doe' })
 }
